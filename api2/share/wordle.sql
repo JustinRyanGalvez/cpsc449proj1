d7209b067de20ef18a8c575f317d12e5cc21f18b
karterL <cadamloya@gmail.com>
2022-10-20 21:09:24 -0700

    Added more stuff to the game table

--- api2/share/wordle.sql
+++ api2/share/wordle.sql
@@ -1,8 +1,8 @@
 PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS game;
 DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS game;
 CREATE TABLE user (
 user_id INTEGER PRIMARY KEY AUTOINCREMENT,
 username varchar not null,
@@ -10,8 +10,7 @@ password varchar not null);
 CREATE TABLE game (
 game_id INTEGER PRIMARY KEY AUTOINCREMENT,
 user_id INTEGER,
-word_id INTEGER, 
-guesses_left INTEGER,
+word_id INTEGER, guesses_left INTEGER, guess STRING (5), guess_valid BOOL, correct_spot STRING, wrong_spot STRING,
 FOREIGN KEY (user_id) REFERENCES user (user_id),
 FOREIGN KEY (word_id) REFERENCES answers (word_id));
 CREATE TABLE answers (
