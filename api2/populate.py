24b4c957741791855853ae9f79261c3cd169a670
karterL <cadamloya@gmail.com>
2022-10-21 22:15:26 -0700

    Created the python file to populate the answers table

---
+++ api2/populate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sqlite3
+import json
+#This establishes the connection to wordle.db
+#Make sure to keep in the same directoy as wordle.db or else
+#a new database will be created instead
+conn = sqlite3.connect('wordle.db')
+c = conn.cursor()
+
+def population():
+    #make sure correct.json and valid.json are in the same directory as
+    #populate.py or else there will be errors
+    with open('correct.json',) as e:
+        info = json.load(e)
+        for j in range(len(info)):
+            c.execute("INSERT INTO answers(correct_answers) VALUES(?)",[info[j]])
+    with open('valid.json',) as f:
+        data = json.load(f)
+        for i in range(len(data)):
+            c.execute("INSERT INTO answers(possible_answers) VALUES(?)",[data[i]])
+    #This closes any connection to the database
+    conn.commit()
+    c.close()
+    conn.close()
+
+population()
--- api2/share/wordle.sql
+++ api2/share/wordle.sql
@@ -1,26 +1,28 @@
 PRAGMA foreign_keys=ON;
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS game;
-DROP TABLE IF EXISTS answers;
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS answers;
+DROP TABLE IF EXISTS game;
 CREATE TABLE user (
 user_id INTEGER PRIMARY KEY AUTOINCREMENT,
 username varchar not null,
 password varchar not null);
-CREATE TABLE game (
-game_id INTEGER PRIMARY KEY AUTOINCREMENT,
-user_id INTEGER,
-word_id INTEGER,
-guesses_left INTEGER, 
-guess STRING (5), 
-guess_valid BOOL, 
-correct_spot STRING, 
-wrong_spot STRING,
-FOREIGN KEY (user_id) REFERENCES user (user_id),
-FOREIGN KEY (word_id) REFERENCES answers (word_id));
 CREATE TABLE answers (
 word_id INTEGER PRIMARY KEY AUTOINCREMENT,
 correct_answers varchar not null,
 possible_answers varchar not null);
+CREATE TABLE game (
+game_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+user_id INT NOT NULL,
+word_id INT NOT NULL,
+guess STRING (5) DEFAULT 'null',
+guesses_left INT DEFAULT 6,
+correct_spot STRING (5) DEFAULT 'null',
+wrong_spot STRING (5) DEFAULT 'null',
+condition STRING DEFAULT 'IP',
+FOREIGN KEY (user_id) REFERENCES user (user_id),
+FOREIGN KEY (word_id) REFERENCES answers (word_id));
+INSERT INTO game VALUES(1,1,1,'hello',6,'null','null','IP');
 DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('game',1);
 COMMIT;
